452a4b83d85abd47011fe90c69a4f9ef4f5196d0
Sandeep LS <110336567+SandeepLS@users.noreply.github.com>
2025-01-22 22:09:14 +0530

    1st commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require('cors'); // Import CORS
 require('dotenv').config(); // Load environment variables
 const connectDB = require('./config/db');
 
-// Import routes
+// Import Routes
 const authRoutes = require('./routes/authRoutes'); 
 const taskRoutes = require('./routes/taskRoutes');
 
